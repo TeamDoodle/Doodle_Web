58714b6dfa51820daca0ae33420023e75344d3f4
manhyuk <manhyuk@kw.ac.kr>
2019-03-24 18:28:17 +0900

    스캐폴딩 추가

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import './Root.css';
 
-class App extends Component {
+class Root extends Component {
   render() {
     return (
       <div className="App">
@@ -25,4 +25,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Root;
---
+++ src/components/SignIn/SignIn.tsx
@@ -0,0 +1,10 @@
+import react, {Component} from 'react'
+
+
+
+export class SignIn extends Component {
+
+    render() {
+        return {};
+    }
+}
